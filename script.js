3ac3a89d6442339d800f4c4fefe31ad73af990a9
undercloud <lodashes@gmail.com>
2017-12-17 23:48:19 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,138 +1,202 @@
-		
-CRMBusinessProcess = {}
+"use strict";        
 
+function Edito(options) {
+    options = options || {};
+    options.tabSize = options.tabSize || 4;
+    options.syntax = options.syntax || 'nohighlight';
 
-		// new Blob(["😀"]).size
+    if (typeof options.spellcheck === "undefined") {
+        options.spellcheck = false;
+    }
 
-		$('#business-code-editor').keydown(function(e){
-			var keyCode = e.keyCode || e.which;
-		
-			if(keyCode == 9){
-				e.preventDefault();
-				
-				var start = this.selectionStart;
-				var end = this.selectionEnd;
+    options.tab = options.usingSpaces ? new Array(options.tabSize + 1).join(' ') : "\t";
 
-				this.value = (
-					this.value.substring(0, start)
-					+ "\t"
-					+ this.value.substring(end)
-				);
+    options.mount.innerHTML = (
+        '<div class="edito' + (options.theme === "dark" ? ' edito--theme-dark' : '') + '">' +
+            '<div class="edito__lines"></div>' +
+            '<div class="edito__wrap">' +
+                '<textarea class="edito__textarea"></textarea>' +
+                '<pre class="edito__pre">' +
+                    '<code class="edito__code hljs"></code>' +
+                '</pre>' +
+            '</div>' +
+            '<div class="edito__code-status">' +
+                '<div class="edito__position"></div>' +
+            '</div>' +
+        '</div>'
+    );
 
-				this.selectionStart = this.selectionEnd = start + 1;
-			}
+    var api = {
+        diff: function (elem) {
+            return elem.scrollTop / elem.scrollHeight * 100;
+        },
+        abs: function (elem, abs) {
+            return elem.scrollHeight /  100 * abs; 
+        },
+        scroll: function() {
+            //var abs = api.diff(this);
+            
+            console.log(this.scrollHeight,codeView.scrollHeight)
 
-			if (keyCode == 13) {
-				//this.onscroll(this);
-			}
-		}).on('paste',function(){
-			setTimeout(function(thisis){
-				CRMBusinessProcess.editor.onkeypress.call(CRMBusinessProcess.editor,thisis)
-			},50,this)
-		}).on('click',function(){
-			CRMBusinessProcess.editor.highlightLine();
-		})
-	
-		var codeEditor = document.getElementById('business-code-editor');
-		var codeView = document.getElementById('business-code-view');
-		var codeLines = document.getElementById('business-code-lines');
-		var codeStatus = document.getElementById('business-code-status');
+            codeView.scrollTop = this.scrollTop;
+            codeView.scrollLeft = this.scrollLeft;
+            codeLines.scrollTop = this.scrollTop;
+        },
+        keyup: function (e) {
+            codeView.innerText = this.value;    
+            hljs.highlightBlock(codeView);
+            api.buildLines();
+            api.highlightLine();
 
-		CRMBusinessProcess.editor = {
-			onkeypress: function(self){
-				var source = self.value;
-				codeView.innerText = source;
-				
-				hljs.highlightBlock(codeView);
-				this.buildLines(source),
+            var keyCode = e.keyCode || e.which;
+            if (keyCode == 13) {
+                api.scroll.call(this);
+            }
+        },
+        keydown: function (e) {
+            var keyCode = e.keyCode || e.which;
 
-				this.highlightLine();
-			},
-			onscroll: function(self){
-				codeView.scrollTop = self.scrollTop; 
-				codeView.scrollLeft = self.scrollLeft;
-				codeLines.scrollTop = self.scrollTop;
-			},
-			buildLines: function(source){
-				var lines = source.split('\n').length;
+            if (keyCode === 9) {
+                e.preventDefault();
+                
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
 
-				var num = '';
-				for(var i = 0;i<lines;i++){
-					num += '<div class="business-code-lines-item" business-code-line="' + (i + 1) + '">' + (i + 1) +'</div>';
-				}
+                this.value = (
+                    this.value.substring(0, start)
+                    + options.tab
+                    + this.value.substring(end)
+                );
 
-				codeLines.innerHTML = num;
-			},
-			getCurrentPos: function(){
-				var pos = 0;
+                this.selectionStart = this.selectionEnd = start + options.tab.length;
+            }
+        },
+        buildLines: function () {
+            var lines = codeEditor.value.split(end).length;
 
-				if (codeEditor.selectionStart) {
-					pos = codeEditor.selectionStart;
-				} else if (document.selection) {
-					codeEditor.focus();
+            var num = '';
+            for (var i = 1; i <= lines; i++) {
+                num += '<div class="edito__lines-item" data-code-line="' + i + '">' + i +'</div>';
+            }
 
-					var r = document.selection.createRange();
-					if (r == null) {
-						pos = 0;
-					}
+            //num += '<div class="edito__lines-item">&nbsp;</div>';
 
-					var re = codeEditor.createTextRange(),
-						rc = re.duplicate();
-					re.moveToBookmark(r.getBookmark());
-					rc.setEndPoint('EndToStart', re);
+            codeLines.innerHTML = num;
+        },
+        getCurrentPos: function () {
+            var pos = 0;
 
-					pos = rc.text.length;
-				}else{
-					pos = 0;
-				}
-				
-				var range = codeEditor.value.substr(0,pos);
-				var line = range.split('\n').length;
-				var col = pos - range.lastIndexOf('\n');
-				
-				return {
-					line: line,
-					col: col
-				};
-			},
-			highlightLine: function(){
-				var pos = this.getCurrentPos();
-				
-				codeStatus.innerText = 'Line ' + pos.line + ', Column ' + pos.col;
-				
-				$('#business-code-lines')
-					.find('.business-code-lines-item.cursor')
-					.removeClass('cursor');
-					
-				$('#business-code-lines')
-					.find('.business-code-lines-item[business-code-line="' + pos.line + '"]')
-					.addClass('cursor')
-			},
-			highlightErrorLine: function(line){
-				$('#business-code-lines')
-					.find('.business-code-lines-item.error')
-					.removeClass('error');
-				
-				if(!line) return;
-				
-				$('#business-code-lines')
-					.find('.business-code-lines-item[business-code-line="' + line + '"]')
-					.addClass('error')
-			},
-		}
+            if (codeEditor.selectionStart) {
+                pos = codeEditor.selectionStart;
+            } else if (document.selection) {
+                codeEditor.focus();
 
-		var script = document.createElement('script');
-		
-		script.src = "code.highlight.pack.js";
-		script.onload = function(){
-			$('#business-code-editor').trigger('keyup');
-		}
+                var r = document.selection.createRange();
+                if (r === null) {
+                    pos = 0;
+                }
 
-		var css = document.createElement('link');
-		
-		css.rel = 'stylesheet';
-		css.type = 'text/css';
-		css.href = 'code.highlight.pack.css';
-		
-		document.head.appendChild(css)
-		document.head.appendChild(script);
+                var re = codeEditor.createTextRange(),
+                    rc = re.duplicate();
+                re.moveToBookmark(r.getBookmark());
+                rc.setEndPoint('EndToStart', re);
+
+                pos = rc.text.length;
+            }else{
+                pos = 0;
+            }
+            
+            var range = codeEditor.value.substr(0,pos);
+            var line = range.split(end).length;
+            var column = pos - range.lastIndexOf(end);
+            
+            return {
+                line: line,
+                column: column
+            };
+        },
+        highlightLine: function(position){
+            var pos = position || this.getCurrentPos();
+            
+            codePosition.innerText = 'Line ' + pos.line + ' Column ' + pos.column;
+            
+            var cursor = codeLines.querySelector('.edito__cursor');
+            if (cursor) {
+                cursor.classList.remove('edito__cursor');
+            }
+
+            cursor = codeLines.querySelector('.edito__lines-item[data-code-line="' + pos.line + '"]');
+            if (cursor) {
+                cursor.classList.add('edito__cursor');
+            } else {
+                this.highlightLine({line: 1, column: 1});
+            }
+        }
+    };
+
+    var end = "\n";
+
+    var codeEditor = options.mount.querySelector('.edito__textarea');
+    var codeView = options.mount.querySelector('.edito__code');
+    var codeLines = options.mount.querySelector('.edito__lines');
+    var codePosition = options.mount.querySelector('.edito__position');
+
+    codeEditor.spellcheck = options.spellcheck;
+
+    codeEditor.style.tabSize = options.tabSize;
+    codeView.style.tabSize = options.tabSize;
+    codeEditor.style['-moz-tab-size'] = options.tabSize;
+    codeView.style['-moz-tab-size'] = options.tabSize;
+
+    if (options.syntax) {    
+        codeView.classList.add(options.syntax);
+    }
+
+    codeEditor.addEventListener('keydown', api.keydown);
+    codeEditor.addEventListener('keyup', api.keyup);
+    codeEditor.addEventListener('paste', api.keydown);
+    codeEditor.addEventListener('scroll', api.scroll);
+    codeEditor.addEventListener('click', function(){
+        api.highlightLine();
+    });
+
+    api.keyup.call(codeEditor, {keyCode: -1});
+
+    Edito.prototype.gotoLine = function (line) {
+        api.highlightLine({line: line, column: 1});
+        codeEditor.scrollTop = (line * parseInt(window.getComputedStyle(codeEditor).lineHeight)) - (codeEditor.offsetHeight / 2);
+    };
+
+    Edito.prototype.highlightErrorLine = function (line) {
+        var error = codeLines.querySelector('.edito__error-line');
+
+        if (error) {
+            error.classList.remove('edito__error-line');
+        }
+
+        if (!line) {
+            return;
+        }
+
+        this.gotoLine(line);
+
+        error = codeLines.querySelector('.edito__lines-item[data-code-line="' + line + '"]');
+
+        if (error) {
+            error.classList.add('edito__error-line');
+        }
+    };
+
+    Edito.prototype.clearErrorLine = function () {
+        this.highlightErrorLine(0);
+    };
+
+    Edito.prototype.setValue = function (value) {
+        codeEditor.value = value;
+        api.keyup.call(codeEditor, {keyCode: -1});
+    };
+
+    Edito.prototype.getValue = function () {
+        return codeEditor.value;
+    };
+}
